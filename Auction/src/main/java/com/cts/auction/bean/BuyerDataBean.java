e999cb13f3932ab10ee64046307d62c5615a7f3e
MuralidaranSelvamani <98650678+MuralidaranSelvamani@users.noreply.github.com>
2022-01-29 21:35:34 +0530

    Initial Commit

--- Auction/src/main/java/com/cts/auction/bean/BuyerDataBean.java
+++ Auction/src/main/java/com/cts/auction/bean/BuyerDataBean.java
@@ -2,4 +2,79 @@ package com.cts.auction.bean;
 
 public class BuyerDataBean {
 
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public String pin;
+	public String phone;
+	public String email;
+	public String productId;
+	public String bidAmount;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getBidAmount() {
+		return bidAmount;
+	}
+	public void setBidAmount(String bidAmount) {
+		this.bidAmount = bidAmount;
+	}
+	
+	
+	
 }
--- Auction/src/main/java/com/cts/auction/bean/ProductDataBean.java
+++ Auction/src/main/java/com/cts/auction/bean/ProductDataBean.java
@@ -2,4 +2,49 @@ package com.cts.auction.bean;
 
 public class ProductDataBean {
 
+	public String productName;
+	public String shortDesc;
+	public String detailedDesc;
+	public String category;
+	public String startingPrice;
+	public String bidEndDate;
+	
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getShortDesc() {
+		return shortDesc;
+	}
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
+	}
+	public String getDetailedDesc() {
+		return detailedDesc;
+	}
+	public void setDetailedDesc(String detailedDesc) {
+		this.detailedDesc = detailedDesc;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getStartingPrice() {
+		return startingPrice;
+	}
+	public void setStartingPrice(String startingPrice) {
+		this.startingPrice = startingPrice;
+	}
+	public String getBidEndDate() {
+		return bidEndDate;
+	}
+	public void setBidEndDate(String bidEndDate) {
+		this.bidEndDate = bidEndDate;
+	}
+	
+	
 }
